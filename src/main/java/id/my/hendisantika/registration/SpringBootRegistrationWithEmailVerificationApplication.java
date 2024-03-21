3ecddf6a29ae3861d8ba57f54996658adfec24ff
hendisantika <hendisantika@yahoo.co.id>
2024-03-22 06:58:37 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/registration/SpringBootRegistrationWithEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegistrationWithEmailVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRegistrationWithEmailVerificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/registration/SpringBootRegistrationWithEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationWithEmailVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
