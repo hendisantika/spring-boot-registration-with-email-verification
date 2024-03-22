88a7f9b7281a47b129aa4e9b91cc8a25cf0cc940
hendisantika <hendisantika@yahoo.co.id>
2024-03-22 07:04:56 +0700

    Add ConfirmationToken Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/registration/entity/ConfirmationToken.java
@@ -0,0 +1,58 @@
+package id.my.hendisantika.registration.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-registration-with-email-verification
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/22/24
+ * Time: 07:03
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "confirmationToken")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "token_id")
+    private Long tokenId;
+
+    @Column(name = "confirmation_token")
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public ConfirmationToken(User user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+
+}
