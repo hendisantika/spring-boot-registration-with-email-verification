ba98be9ee8131fec66b0862035fe987b302c3694
hendisantika <hendisantika@yahoo.co.id>
2024-03-22 07:06:27 +0700

    Add EmailService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/registration/service/EmailService.java
+++ src/main/java/id/my/hendisantika/registration/service/EmailService.java
@@ -1,6 +1,9 @@
 package id.my.hendisantika.registration.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +19,16 @@ import org.springframework.stereotype.Service;
 @Service("emailService")
 public class EmailService {
 
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        javaMailSender.send(email);
+    }
+
 }
