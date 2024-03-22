c3fb0fba014f2fbdd864756db50200e9730be954
hendisantika <hendisantika@yahoo.co.id>
2024-03-22 07:12:29 +0700

    Add registerUser in UserController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/registration/controller/UserController.java
+++ src/main/java/id/my/hendisantika/registration/controller/UserController.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.registration.controller;
 
+import id.my.hendisantika.registration.entity.User;
 import id.my.hendisantika.registration.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,6 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("users")
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
 }
